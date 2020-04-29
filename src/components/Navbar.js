a4c4f4ba4277d94e58686e1bcef5650188ccc819
Paradorn Pimporn <paraz@Paradorns-MacBook-Pro.local>
2020-04-29 21:29:34 +0700

    Using Axios

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,9 +6,7 @@ const Navbar = (props) => {
   return (
     <nav>
       <div className="nav-wrapper">
-        <a href="#" className="brand-logo">
-          Logo
-        </a>
+        <a className="brand-logo">Logo</a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
           <li>
             <Link to="/">Home</Link>
