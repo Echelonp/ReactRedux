2f67a9dcf7d4308695594fc745a3bf8775f8675e
Paradorn Pimporn <paraz@Paradorns-MacBook-Pro.local>
2020-04-29 19:12:18 +0700

    #26 Links & NavLinks

---
+++ src/components/Navbar.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div class="nav-wrapper">
+        <a href="#" class="brand-logo">
+          Logo
+        </a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <NavLink to="/about">About</NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact">Contact</NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
